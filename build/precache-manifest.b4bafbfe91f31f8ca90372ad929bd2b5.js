afc1418a5001cad0f53098b6128223db70950423
ErikBoerma <eboerma@gmail.com>
2020-02-26 21:44:30 -0500

    attempting to launch portfolio

---
+++ build/precache-manifest.b4bafbfe91f31f8ca90372ad929bd2b5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e93f2042a2f30002cbb5f3876bb315f5",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "686c8db9cf2d443ed2c4",
+    "url": "/react-portfolio/static/css/2.0dbf18b6.chunk.css"
+  },
+  {
+    "revision": "618aee914b666fee098e",
+    "url": "/react-portfolio/static/css/main.71296192.chunk.css"
+  },
+  {
+    "revision": "686c8db9cf2d443ed2c4",
+    "url": "/react-portfolio/static/js/2.1706bed2.chunk.js"
+  },
+  {
+    "revision": "618aee914b666fee098e",
+    "url": "/react-portfolio/static/js/main.5c04775b.chunk.js"
+  },
+  {
+    "revision": "94872cf7e81768d148a3",
+    "url": "/react-portfolio/static/js/runtime~main.f974a8eb.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/react-portfolio/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/react-portfolio/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/react-portfolio/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/react-portfolio/static/media/slick.f97e3bbf.svg"
+  }
+]);
